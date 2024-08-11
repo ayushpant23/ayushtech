f7e232d6857e20057d4c2ebf83b0e671f0e508fe
Ayush Pant <pantayush012@gmail.com>
2024-08-11 13:21:59 +0530

    initail commit

---
+++ ayushtech/src/main/java/com/ays/ayushtech/AyushtechApplication.java
@@ -0,0 +1,13 @@
+package com.ays.ayushtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyushtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyushtechApplication.class, args);
+	}
+
+}
---
+++ ayushtech/src/test/java/com/ays/ayushtech/AyushtechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ays.ayushtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyushtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
